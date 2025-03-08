2dafe2272e53a52bec7f85def241d2ecaf749ae3
alaa-gnt <oa_geunat@esi.dz>
2025-03-08 02:13:53 +0100

    adding the items to main

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
-   
-  const Homepage({super.key});
+  const Homepage({super.key}); 
+  
 
   @override
   Widget build(BuildContext context) {
--- lib/Pages/ShopPage.dart
+++ lib/Pages/ShopPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:e_comm_simple/componantes/Item.dart';
 class ShopPage extends StatefulWidget {
    
   const ShopPage({super.key});
@@ -35,11 +35,35 @@ class _ShopPageState extends State<ShopPage> {
           children: [
             // that hard thingy 
 
-            Text("Hot Picks"), 
+            Text("Hot Picks"),
+                        ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            Item(
+                              name: "Nike Air Max 90",
+                              image: "lib/images/airmax.jpg",
+                              discription: "The Nike Air Max 90 stays true to its OG roots with its iconic Waffle outsole, stitched overlays and classic, TPU accents. Fresh colors give a modern look while Max Air cushioning adds comfort to your journey.",
+                              price: 120.0,
+                            ),
+                            Item(
+                              name: "Nike Air Max 97",
+                              image: "lib/images/airmax97.jpg",
+                              discription: "The Nike Air Max 97 keeps a sneaker icon going strong with the same design details that made it famous: water-ripple lines, reflective piping and full-length Max Air cushioning.",
+                              price: 170.0,
+                            ),
+                            Item(
+                              name: "Nike Air Max 270",
+                              image: "lib/images/airmax270.jpg",
+                              discription: "The Nike Air Max 270 delivers visible cushioning under every step. Updated for modern comfort, it pays homage to the original 1991 Air Max 180 with an exaggerated tongue top and heritage logo.",
+                              price: 150.0,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+            }
 
-          ],
-        ),),
           
-    );
-  }
-}
